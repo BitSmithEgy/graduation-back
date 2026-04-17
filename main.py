e7a3577d167bfc15825679d73e51af3196afe7d9
BitSmith <106936064+BitSmithEgy@users.noreply.github.com>
2026-04-17 13:32:12 +0200

    Both cookie & header auth

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import users
 import database
 
 database.init_db()
 app = FastAPI(title="Graduation Project API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
